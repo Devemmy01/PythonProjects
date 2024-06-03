ccff366fbfda0603a8f81a4984f6b37812a8f5e2
Emmanuel Balogun <emmanx25@gmail.com>
2024-06-03 22:55:19 +0100

    Added paddle movement functionality to Pong game

--- Pong.py
+++ Pong.py
@@ -26,6 +26,43 @@ paddleB.color("white")
 paddleB.penup()
 paddleB.goto(350, 0)
 
+# Ball
+ball = turtle.Turtle()
+ball.speed(0)
+ball.shape("circle")
+ball.color("white")
+ball.penup()
+ball.goto(0, 0)
+
+# Paddle movement (Function)
+def paddle_a_up():
+    y = paddleA.ycor() #gets the y coordinate of the first paddle
+    y += 20 #add 20 to the y coordinate
+    paddleA.sety(y) #set y of the paddle to the new y coordinate  
+
+def paddle_a_down():
+    y = paddleA.ycor() #gets the y coordinate of the first paddle
+    y -= 20 #subtract 20 to the y coordinate
+    paddleA.sety(y) #set y of the paddle to the new y coordinate
+
+def paddle_b_up():
+    y = paddleB.ycor() #gets the y coordinate of the second paddle
+    y += 20 #add 20 to the y coordinate
+    paddleB.sety(y) #set y of the paddle to the new y coordinate  
+
+def paddle_b_down():
+    y = paddleB.ycor() #gets the y coordinate of the first paddle
+    y -= 20 #subtract 20 to the y coordinate
+    paddleB.sety(y) #set y of the paddle to the new y coordinate
+
+    
+# Keyboard binding
+wn.listen()
+wn.onkeypress(paddle_a_up, "w")    
+wn.onkeypress(paddle_a_down, "s")    
+wn.onkeypress(paddle_b_up, "Up")    
+wn.onkeypress(paddle_b_down, "Down")    
+
 # Main loop
 while True:
     wn.update()
